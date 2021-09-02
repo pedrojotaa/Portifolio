736eb2733cbb77c78ba81d2ca7c606504aefc090
Joao Pedro <eng.joao.pedro@outlook.com>
2021-09-02 10:22:47 -0300

    section with bootStrp and functional  menu with js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let menuMobile = document.querySelector('.menu-mobile')
 let body = document.querySelector('body')
 
+//ao clicar no botao o menu aparece e o icone muda
+
 menuMobile.addEventListener('click', ()=>{
     //verifica se no menu-mobile contem a classe bi-list
     if(menuMobile.classList.contains('bi-list')){
@@ -10,4 +12,17 @@ menuMobile.addEventListener('click', ()=>{
     }
     //adiciona uma nova classe ao body
     body.classList.toggle('menu-nav-active')
+})
+
+//ao clicar nos botões do menu o menu desaparece e o icone muda
+
+let navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+    item.addEventListener('click', ()=>{
+        if(body.classList.contains('menu-nav-active')){
+            body.classList.remove('menu-nav-active')
+            menuMobile.classList.replace('bi-x', 'bi-list')
+        }
+    })
 })

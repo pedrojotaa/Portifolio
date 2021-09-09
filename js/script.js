002aef43b32ea94057d5f9b8307b0ce771edf295
Joao Pedro <eng.joao.pedro@outlook.com>
2021-09-08 22:19:45 -0300

    adding animation

--- js/script.js
+++ js/script.js
@@ -26,3 +26,28 @@ navItem.forEach(item =>{
         }
     })
 })
+
+//criando animação
+
+const item = document.querySelectorAll('[data-anime]')
+
+const animaScroll = () => {
+    //tamanho do topo da tela no momento em relação a tela total no eixo Y
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85
+
+    item.forEach(element =>{
+        //se o topo da pagina > altura que o elemento esta do topo
+        if(windowTop > element.offsetTop){
+            element.classList.add('animation')
+        }else{
+            element.classList.remove('animation')
+        }
+    })
+}
+//sempre fica ligado
+animaScroll()
+
+//funcção de callback
+window.addEventListener('scroll', () => {
+    animaScroll()
+})
